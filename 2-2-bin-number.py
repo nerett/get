550e71d3d00ebfdf3d406d7920c157b58860acc3
nerett <pt-mvb@inbox.lv>
2022-03-09 10:53:21 +0300

    finished 2-2

--- 2-2-bin-number.py
+++ 2-2-bin-number.py
@@ -15,11 +15,11 @@ number_256 = [ 0, 0, 0, 0, 0, 0, 0, 0 ] #переполнение
 
 gpio.setup( dac, gpio.OUT )
 
-#gpio.output( dac, number_255 )
+gpio.output( dac, number_255 )
 #gpio.output( dac, number_127 )
 #gpio.output( dac, number_64 )
 #gpio.output( dac, number_32 )
-gpio.output( dac, number_5 )
+#gpio.output( dac, number_5 )
 #gpio.output( dac, number_0 )
 #gpio.output( dac, number_256 )
 

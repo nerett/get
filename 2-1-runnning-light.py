1eb1ad11a27ccc459aee55e4ba4759ca8ffd237e
nerett <pt-mvb@inbox.lv>
2022-03-09 10:07:19 +0300

    finished 2-1

--- 2-1-runnning-light.py
+++ 2-1-runnning-light.py
@@ -1,6 +1,19 @@
-import RPI.GPIO as gpio
+import RPi.GPIO as gpio
 import time
 
 gpio.setmode( gpio.BCM )
-gpio.setup( 26, gpio.OUT )
-gpio.output( 26, 1 )
+
+#gpio.setup( 26, gpio.OUT )
+#gpio.output( 26, 1 )
+
+leds = [ 21, 20, 16, 12, 7, 8, 25, 24 ]
+gpio.setup( leds, gpio.OUT )
+
+for i in range( 3 ):
+    for k in leds:
+        gpio.output( k, 1 )
+        time.sleep( 0.2 )
+        gpio.output( k, 0 )
+        time.sleep( 0.2 )
+
+gpio.cleanup()
